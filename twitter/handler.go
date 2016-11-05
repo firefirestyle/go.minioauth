20ff6064b33d4e1bc632058e4daa13130100efef
kyorohiro <kyorohiro@gmail.com>
2016-11-05 23:37:54 +0900

    ..

--- twitter/handler.go
+++ twitter/handler.go
@@ -6,15 +6,10 @@ import (
 
 	"errors"
 
-	"crypto/sha1"
-	"encoding/base64"
-	"io"
-
-	"strings"
-
-	"github.com/firefirestyle/go.miniprop"
+	"github.com/firefirestyle/go.minioauth/sns"
 	"google.golang.org/appengine"
 	//	"google.golang.org/appengine/log"
+
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/log"
 )
@@ -94,19 +89,14 @@ func (obj *TwitterHandler) MakeUrlFailedToMakeToken(baseAddr string) (string, er
 
 func (obj *TwitterHandler) HandleLoginEntry(w http.ResponseWriter, r *http.Request) {
 	clCallbackUrl := r.URL.Query().Get(UrlOptCallbackUrl)
-	//ctx := appengine.NewContext(r)
-	//Debug(ctx, "HandleLoginEntry")
-	//
 	// make redirect URL
 	if clCallbackUrl == "" {
-		//Debug(ctx, "HandleLoginEntry callback error")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	//
 	clCallbackUrlObj, clCallbackUrlErr := url.Parse(clCallbackUrl)
 	if clCallbackUrlErr != nil {
-		//Debug(ctx, "HandleLoginEntry parse error")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -114,7 +104,6 @@ func (obj *TwitterHandler) HandleLoginEntry(w http.ResponseWriter, r *http.Reque
 	//
 	opts, optsErr := obj.onEvent.OnRequest(w, r, obj)
 	if optsErr != nil {
-		//Debug(ctx, "HandleLoginEntry onRequest err")
 		tmpValues := clCallbackUrlObj.Query()
 		if opts != nil {
 			for k, v := range opts {
@@ -124,101 +113,61 @@ func (obj *TwitterHandler) HandleLoginEntry(w http.ResponseWriter, r *http.Reque
 		clCallbackUrlObj.RawQuery = tmpValues.Encode()
 		http.Redirect(w, r, clCallbackUrlObj.String(), http.StatusFound)
 		return
-	} else {
-		//
-		svCallbackUrlObj, _ := url.Parse(obj.config.CallbackUrl)
-		if svCallbackUrlObj.Path == clCallbackUrlObj.Path {
-			//Debug(ctx, "HandleLoginEntry config parse err")
-
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		//
-		tmpValues := svCallbackUrlObj.Query()
-		tmpValues.Add(UrlOptCallbackUrl, clCallbackUrl)
-
-		if opts != nil {
-			for k, v := range opts {
-				tmpValues.Add(k, v)
-			}
-		}
-		//
-		{
-			publicSign := miniprop.MakeRandomId()
-			tmpValues.Add("ps", publicSign)
-
-			hash := sha1.New()
-			io.WriteString(hash, publicSign)
-			io.WriteString(hash, opts["kw"])
-			io.WriteString(hash, opts["kv"])
-			io.WriteString(hash, clCallbackUrlObj.String())
-			io.WriteString(hash, obj.config.SecretSign)
-			calcHash := base64.StdEncoding.EncodeToString(hash.Sum(nil))
-			tmpValues.Add("hash", calcHash)
-		}
-		//
-		svCallbackUrlObj.RawQuery = tmpValues.Encode()
-		//
-		//
-		redirectUrl := ""
-
-		twitterObj := obj.twitterManager.NewTwitter()
-		oauthResult, err := twitterObj.SendRequestToken(appengine.NewContext(r), svCallbackUrlObj.String())
-		if err != nil {
-			//Debug(ctx, "HandleLoginEntry make token errr :"+err.Error())
-
-			failedOAuthUrl, _ := obj.MakeUrlFailedToMakeToken(clCallbackUrl)
-			redirectUrl = failedOAuthUrl
-		} else {
-			//Debug(ctx, "HandleLoginEntry config ok log :"+oauthResult.GetOAuthTokenUrl())
-
-			redirectUrl = oauthResult.GetOAuthTokenUrl()
-		}
-		//
-		// Do Redirect
-		http.Redirect(w, r, redirectUrl, http.StatusFound)
 	}
+	//
+	svCallbackUrlObj, _ := url.Parse(obj.config.CallbackUrl)
+	if svCallbackUrlObj.Path == clCallbackUrlObj.Path {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	//
+	//
+	tmpValues := svCallbackUrlObj.Query()
+	tmpValues.Add(UrlOptCallbackUrl, clCallbackUrl)
+
+	for k, v := range opts {
+		tmpValues.Add(k, v)
+	}
+	//
+	sns.WithHashAndValue(tmpValues, obj.config.SecretSign, clCallbackUrlObj.String(), opts)
+	//
+	svCallbackUrlObj.RawQuery = tmpValues.Encode()
+	//
+	//
+	redirectUrl := ""
+
+	twitterObj := obj.twitterManager.NewTwitter()
+	oauthResult, err := twitterObj.SendRequestToken(appengine.NewContext(r), svCallbackUrlObj.String())
+	if err != nil {
+		failedOAuthUrl, _ := obj.MakeUrlFailedToMakeToken(clCallbackUrl)
+		redirectUrl = failedOAuthUrl
+	} else {
+		redirectUrl = oauthResult.GetOAuthTokenUrl()
+	}
+	//
+	// Do Redirect
+	http.Redirect(w, r, redirectUrl, http.StatusFound)
+
 }
 
 func (obj *TwitterHandler) HandleLoginExit(w http.ResponseWriter, r *http.Request) {
-	//
-	//
-	// response easy check
-	{
-		values := r.URL.Query()
-		hashV := values.Get("hash")
-		publicSign := values.Get("ps")
-		kw := values.Get("kw")
-		kv := values.Get("kv")
-		clCallback := values.Get("cb")
-		{
-			hash := sha1.New()
-			io.WriteString(hash, publicSign)
-			io.WriteString(hash, kw)
-			io.WriteString(hash, kv)
-			io.WriteString(hash, clCallback)
-			io.WriteString(hash, obj.config.SecretSign)
-			calcHash := base64.StdEncoding.EncodeToString(hash.Sum(nil))
-
-			if strings.Compare(calcHash, hashV) != 0 {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-		}
-	}
-	//
 	callbackUrl := r.URL.Query().Get(UrlOptCallbackUrl)
 	urlObj, err := url.Parse(callbackUrl)
 	if err != nil {
-		removeUrlObj, _ := url.Parse(r.RemoteAddr)
-		query := removeUrlObj.Query()
-		query.Add("error", "error")
-		removeUrlObj.RawQuery = query.Encode()
-		http.Redirect(w, r, removeUrlObj.String(), http.StatusFound)
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	//
+	// response easy check
+	checkErr := sns.CheckHashAndValue(w, r, obj.config.SecretSign)
+	if checkErr != nil {
+		query := urlObj.Query()
+		query.Add("error", checkErr.Error())
+		urlObj.RawQuery = query.Encode()
+		http.Redirect(w, r, urlObj.String(), http.StatusFound)
+		return
+	}
+
 	//
 	twitterObj := obj.twitterManager.NewTwitter()
 	rt, err := twitterObj.OnCallbackSendRequestToken(appengine.NewContext(r), r.URL)
